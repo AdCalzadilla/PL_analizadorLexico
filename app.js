d6e8a621606f91f7db7464a206c669b397367663
Kevin <alu0100774665@ull.edu.es>
2015-03-25 21:41:02 +0000

    Cambios heroku

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+var $ = require('jquery');
+var _ = require('underscore');
+
+app.set('port', (process.env.PORT || 3000));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.set('layout', 'myLayout') // defaults to 'layout'  '
+
+app.use(expressLayouts);
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Analizador Léxico' });
+})
